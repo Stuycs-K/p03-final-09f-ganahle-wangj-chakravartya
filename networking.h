9ffe49a5ca1138a2ad7b806e4b806eb7c03c7ed5
Aritro Chakravarty <aritroc@nycstudents.net>
2026-01-06 01:04:28 -0500

    restructuring, will likely include future functions in header

--- networking.h
+++ networking.h
@@ -6,22 +6,17 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
 #include <string.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <semaphore.h>
 
 #define BUFFER_SIZE 256
 #define PORT "9845"
 #define BACKLOG 5
 
-void err(int i, char s);
-
-int client_tcp_handshake(charip);
+int server_setup();
+int server_tcp_handshake(int listen_socket);
+int client_tcp_handshake(char *server_address);
+void err(int i, char *message);
 
 #endif
